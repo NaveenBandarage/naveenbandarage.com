422e76d5528b452c21fefca3323e67780afaeea5
Naveeen Bandarage <naveen.bandarage@xero.com>
2023-09-10 13:39:52 +1200

    using react helmet instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
 import './App.css';
 import React from 'react';
-import GoogleFontLoader from 'react-google-font-loader';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
- <body>
-  <div id="container">
-    <p>Hey, I'm Naveen.</p>
-    <p>I'm a software engineer currently working at <a target="_blank" id="xero" href="https://www.xero.com/" rel="noreferrer">Xero</a>.</p>
-    <p>I've worked at places like <a target="_blank" id='black' href='https://www.anz.co.nz/' rel="noreferrer">ANZ</a>, <a target="_blank" id='black' href='https://www.myclearhead.com/' rel="noreferrer">Clearhead</a>.</p>
-    <p>
-      You can find me on <a target="_blank" id='black' href='https://www.linkedin.com/in/naveenbandarage/' rel="noreferrer">LinkedIn</a> and  <a target="_blank" id='black' href='http://twitter.com/naveenbandarage' rel="noreferrer">Twitter</a> and  <a target="_blank" id='black' href='https://www.github.com/naveenbandarage/' rel="noreferrer">GitHub</a>.
-    </p>
-    <p>
-       Let's talk at{' '}
-       <a target="_blank" id='black' href='mailto:im@naveenbandarage.com'>im@naveenbandarage.com</a>.
-    </p>
-    <GoogleFontLoader fonts={[{ font: 'Bitter', weights: [400] }]} />
-
-  </div>
-</body>
+    <div>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bitter&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <body>
+        <div id="container">
+          <p>Hey, I'm Naveen.</p>
+          <p>
+            I'm a software engineer currently working at{' '}
+            <a
+              target="_blank"
+              id="xero"
+              href="https://www.xero.com/"
+              rel="noreferrer"
+            >
+              Xero
+            </a>
+            .
+          </p>
+          <p>
+            I've worked at places like{' '}
+            <a target="_blank" id="black" href="https://www.anz.co.nz/" rel="noreferrer">
+              ANZ
+            </a>
+            ,{' '}
+            <a
+              target="_blank"
+              id="black"
+              href="https://www.myclearhead.com/"
+              rel="noreferrer"
+            >
+              Clearhead
+            </a>
+            .
+          </p>
+          <p>
+            You can find me on{' '}
+            <a
+              target="_blank"
+              id="black"
+              href="https://www.linkedin.com/in/naveenbandarage/"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>{' '}
+            and{' '}
+            <a target="_blank" id="black" href="http://twitter.com/naveenbandarage" rel="noreferrer">
+              Twitter
+            </a>{' '}
+            and{' '}
+            <a
+              target="_blank"
+              id="black"
+              href="https://www.github.com/naveenbandarage/"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+          <p>
+            Let's talk at{' '}
+            <a target="_blank" id="black" href="mailto:im@naveenbandarage.com">
+              im@naveenbandarage.com
+            </a>
+            .
+          </p>
+        </div>
+      </body>
+    </div>
   );
 }
 
